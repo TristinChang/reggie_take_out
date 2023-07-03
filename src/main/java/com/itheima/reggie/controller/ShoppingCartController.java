9cb0f4f4c13e500cdb47aac0c7d1f2808af86d6b
zlt <847998986@qq.com>
2023-07-03 16:18:20 +0800

    加入redis，缓存验证码和菜品

---
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,96 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.BaseContext;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.ShoppingCart;
+import com.itheima.reggie.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 购物车
+ */
+@Slf4j
+@RestController
+@RequestMapping("/shoppingCart")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     * @return
+     */
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart){
+        log.info("购物车数据:{}",shoppingCart);
+
+        //1用户id
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+
+        //2判断购物车中是否存在
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,currentId);
+        if (shoppingCart.getDishId() != null){
+            queryWrapper.eq(ShoppingCart::getDishId,shoppingCart.getDishId());
+        }else {
+            queryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        }
+
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+
+        if (cartServiceOne != null){
+            //存在，数量+1
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        }else{
+            //不存在，新增
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+
+        return R.success(cartServiceOne);
+    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list(){
+        log.info("查看购物车...");
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+        queryWrapper.orderByAsc(ShoppingCart::getCreateTime);
+
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+
+        return R.success(list);
+    }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @DeleteMapping("/clean")
+    public R<String> clean(){
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+
+        shoppingCartService.remove(queryWrapper);
+
+        return R.success("清空购物车成功");
+    }
+}
